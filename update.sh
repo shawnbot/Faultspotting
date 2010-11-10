ef0776969e576f15defe74aab2952d11629d9ae3
Shawn Allen <shawn@stamen.com>
2010-11-10 10:29:53 -0800

    don't mkdir -p data anymore

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 DATE=`date "+%Y%m%d.%H"`
 FILE=data/$DATE.csv
-mkdir -p data
 if [ ! -e $FILE ]
 then
     curl --max-redirs 3 "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt" > $FILE
