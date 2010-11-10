70c7c6c69ab3a2e6ae556e617cb24c527bf7bf59
Shawn Allen <shawn@stamen.com>
2010-11-10 10:20:08 -0800

    tweak

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ FILE=data/$DATE.csv
 if [ ! -e $FILE ]
 then
     curl --max-redirs 3 "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt" > $FILE
+    ln -s $FILE data/latest.csv
     cat $FILE | egrep -v "^Src" >> data/all.csv
-    cp $FILE data/latest.csv
     python geojson.py data/latest.csv > data/latest.json
     python geojson.py data/all.csv > data/all.json
 fi
