7a352862df3e86b524baad8b6035d0c4279be1b5
Shawn Allen <shawn@stamen.com>
2010-11-10 10:41:05 -0800

    added --silent flag to curl; added note about simplejson prereq in geojson.py

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ FILE=data/$DATE.csv
 mkdir -p data
 if [ ! -e $FILE ]
 then
-    curl --max-redirs 3 "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt" > $FILE
+    curl --silent --max-redirs 4 "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt" > $FILE
     ln -s $FILE data/latest.csv
     cat $FILE | egrep -v "^Src" >> data/all.csv
     python geojson.py data/latest.csv > data/latest.json
