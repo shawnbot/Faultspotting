326207f898e50e703f7beac0a08a43dd85e1c847
Shawn Allen <shawn@stamen.com>
2011-02-03 13:37:35 -0800

    some cleanup

--- geojson.py
+++ geojson.py
@@ -1,14 +1,8 @@
-import csv, simplejson, optparse, re
+import re
 
-parser = optparse.OptionParser()
-opts, args = parser.parse_args()
-
-input_filename = args.pop(0)
-input = csv.DictReader(open(input_filename))
-
+id_field = 'Eqid'
 lat_field = 'Lat'
 lon_field = 'Lon'
-non_props = [lat_field, lon_field]
 
 num_pat = re.compile('^-?[0-9]*(\.?[0-9]+)?([eE]-?[0-9]+)?$')
 def number(value):
@@ -16,16 +10,29 @@ def number(value):
         return float(value)
     return value
 
-features = []
-for row in input:
-    feature = {'properties': {},
+def row2feature(row):
+    feature = {'type': 'Feature',
+               'id': row[id_field],
+               'properties': {},
                'geometry': {'type': 'Point',
                             'coordinates': [float(row[lon_field]), float(row[lat_field])]}}
     for k in row.keys():
         if not (k in non_props):
             feature['properties'][k] = number(row[k])
-    features.append(feature)
+    return feature
 
-collection = {'type': 'FeatureCollection',
-              'features': features}
-print simplejson.dumps(collection)
+if __name__ == '__main__':
+    import simplejson, csv, optparse
+
+    parser = optparse.OptionParser()
+    opts, args = parser.parse_args()
+
+    input_filename = args.pop(0)
+    input = csv.DictReader(open(input_filename))
+
+    non_props = [lat_field, lon_field]
+
+    features = map(row2feature, input)
+    collection = {'type': 'FeatureCollection',
+                  'features': features}
+    print simplejson.dumps(collection)
--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ mkdir -p data
 if [ ! -e $FILE ]
 then
     curl --silent --max-redirs 4 "http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.txt" > $FILE
-    ln -s $FILE data/latest.csv
+		rm -f data/latest.csv
+    cp $FILE data/latest.csv
     cat $FILE | egrep -v "^Src" >> data/all.csv
     python geojson.py data/latest.csv > data/latest.json
     python geojson.py data/all.csv > data/all.json
